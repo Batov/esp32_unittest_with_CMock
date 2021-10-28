82356ddf96966a06d590a86416f652d8dc2264c4
Nikita Batov <batov@outlook.com>
2021-10-28 16:39:13 +0300

    Add cmock usage

--- component_b/test/test.c
+++ component_b/test/test.c
@@ -1,15 +1,20 @@
 #include "component_b.h"
 #include "unity.h"
+#include "Mockcomponent_a.h"
 
 static const char test_tag[] = "[component_b]";
 
 TEST_CASE("B 1", test_tag)
 {
-    TEST_ASSERT_EQUAL(component_b_add(1), 0);
+    Mockcomponent_a_Init();
+    component_a_increment_ExpectAnyArgsAndReturn(2);
+    TEST_ASSERT_EQUAL(0, component_b_add(1));
 }
 
 TEST_CASE("B 2", test_tag)
 {
+    Mockcomponent_a_Init();
     component_b_init(11);
-    TEST_ASSERT_EQUAL(component_b_add(1), 12);
+    component_a_increment_ExpectAndReturn(12, 13);
+    TEST_ASSERT_EQUAL(12, component_b_add(1));
 }
