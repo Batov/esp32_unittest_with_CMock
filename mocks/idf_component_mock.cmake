624f0a0956c63c7cca2b7242bf42181d3045cf5a
Nikita Batov <batov@outlook.com>
2021-11-08 16:20:52 +0300

    Add idf_component_mock.cmake to implement idf_component_mock()

--- mocks/idf_component_mock.cmake
+++ mocks/idf_component_mock.cmake
@@ -1,12 +1,9 @@
-message(STATUS "Building ${original_component_dir} MOCK ")
-
 list(APPEND REQUIRES "cmock")
-
 set(MOCK_GENERATED_HEADERS "")
 set(MOCK_GENERATED_SRCS "")
 set(MOCK_FILES "")
 set(IDF_PATH $ENV{IDF_PATH})
-set(CMOCK_DIR "${IDF_PATH}/components/cmock/CMock")
+set(CMOCK_DIR "${PROJECT_DIR}/mocks/cmock/CMock")
 set(MOCK_GEN_DIR "${CMAKE_CURRENT_BINARY_DIR}")
 list(APPEND INCLUDE_DIRS "${MOCK_GEN_DIR}/mocks")
 
@@ -23,9 +20,6 @@ idf_component_register(SRCS "${MOCK_GENERATED_SRCS}"
                     REQUIRES ${REQUIRES})
 
 # This command builds the mocks.
-# First, environment variable UNITY_DIR is set. This is necessary to prevent unity from looking in its own submodule
-# which doesn't work in our CI yet...
-# The rest is a straight forward call to cmock.rb, consult cmock's documentation for more information.
 add_custom_command(
     OUTPUT ${MOCK_GENERATED_SRCS} ${MOCK_GENERATED_HEADERS}
     COMMAND ${CMAKE_COMMAND} -E env "UNITY_DIR=${IDF_PATH}/components/unity/unity"
